70c7a089804676ec1af8d888b91399774f6f9f1f
bjyen <bj.yenWUSTL@gmail.com>
2016-02-11 11:54:39 -0600

    Delete this model
    Already write MP model in the main.m

--- MortenenPissaridesModel.m
+++
@@ -1,5 +0,0 @@
-% MP_root
-function MP_root=MortenenPissaridesModel(theta)
-global kappa beta q_theta P mu z b theta delta 
-MP_root= kappa./(beta.*q_theta)-P.*((1-mu)*(z-b)-kappa.*mu.*theta +(1-delta).*kappa./q_theta);
-end
